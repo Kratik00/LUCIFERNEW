86eda244f2ea73bbbfe9f84749033afe910ea92b
Kratik00 <159747619+Kratik00@users.noreply.github.com>
2025-05-02 21:11:56 +0530

    Update core.py

--- core.py
+++ core.py
@@ -367,7 +367,7 @@ async def download_and_decrypt_pdf(url, name, key):
 
 #-----------------------Emoji handler------------------------------------
 
-EMOJIS = ["🦁", "🐶", "🐼", "💥", "🐻‍❄️", "☁️", "🦋"]
+EMOJIS = ["🦁", "🎃", "🐼", "💥", "🐻‍❄️", "🎉", "🦋"]
 emoji_counter = 0  # Initialize a global counter
 
 def get_next_emoji():
@@ -382,7 +382,7 @@ async def send_vid(bot: Client, m: Message,cc,filename,thumb,name,prog):
     emoji = get_next_emoji()
     subprocess.run(f'ffmpeg -i "{filename}" -ss 00:00:02 -vframes 1 "{filename}.jpg"', shell=True)   
     await prog.delete (True)   
-    reply = await m.reply_text(f"**📤 🅤︎Ⓟ︎🅛︎Ⓞ︎🅐︎Ⓓ︎🅘︎Ⓝ︎🅖︎....**\n\n **📦 🅣︎ɪᴛʟⒺ︎ =`{name}`**\n\n**╭━━━━━━━━━◆✯◆━━━━━━━━━╮**\n**⚡ MADE BY : 𝗪𝗢𝗟𝗩𝗘𝗦 🐺**\n**╰━━━━━━━━━◆✯◆━━━━━━━━━╯**")   
+    reply = await m.reply_text(f"**📤 🅤︎Ⓟ︎🅛︎Ⓞ︎🅐︎Ⓓ︎🅘︎Ⓝ︎🅖︎....**\n\n **📦 🅣︎ɪᴛʟⒺ︎ =`{name}`**\n\n**╭━━━━━━━━━◆✯◆━━━━━━━━━╮**\n**⚡ MADE BY : 𝙇𝙐𝘾𝙄𝙁𝙀𝙍 💀**\n**╰━━━━━━━━━◆✯◆━━━━━━━━━╯**")   
     try:   
         if thumb == "no":   
             thumbnail = f"{filename}.jpg"   
